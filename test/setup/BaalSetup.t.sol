d33e104ad720570c0b521e0ee8e092cf3eb6beeb
jordan <jordan.lesich@gmail.com>
2024-08-18 16:51:43 -0700

    dep hell escaped, minimum baal setup achieved

--- test/setup/BaalSetup.t.sol
+++ test/setup/BaalSetup.t.sol
@@ -1,20 +1,33 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {IBaal} from "baal/interfaces/IBaal.sol";
-import {Test} from "forge-std/Test.sol";
+import {IBaal} from "lib/Baal/contracts/interfaces/IBaal.sol";
+import {Test, console2} from "lib/forge-std/src/Test.sol";
+import {Accounts} from "./Accounts.t.sol";
 
-contract BaalSetup is Test {
+contract BaalSetup is Test, Accounts {
     uint256 constant BLOCK_NUMBER = 244299097;
     address constant DAO_MASONS = 0x5B448757A34402DEAcC7729B79003408CDfe1438;
 
     IBaal internal _baal;
 
-    function _setupBaal() internal {
+    function setUp() public {
         vm.createSelectFork(vm.rpcUrl("arbitrumOne"), BLOCK_NUMBER);
 
         _baal = IBaal(DAO_MASONS);
+        address[] memory shamans = new address[](1);
+        uint256[] memory permissions = new uint256[](1);
+        shamans[0] = someGuy();
+        permissions[0] = 2;
 
-        uint256 totalLoot = _baal.totalLoot();
+        vm.prank(_baal.avatar());
+
+        _baal.setShamans(shamans, permissions);
+    }
+
+    function test() public {
+        uint256 permission = _baal.shamans(someGuy());
+
+        console2.log("permission", permission);
     }
 }
--- test/setup/HatsSetup.t.sol
+++ test/setup/HatsSetup.t.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {Hats} from "hats-protocol/src/Hats.sol";
+import {Hats} from "lib/hats-protocol/src/Hats.sol";
 
 contract HatsSetup {
     Hats internal _hats;
 
     function _setupHats() internal {
-        _hats = new Hats();
+        // _hats = new Hats();
     }
 }
