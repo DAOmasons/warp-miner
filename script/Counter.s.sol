8ef1d4f2f8635100544530654999032ee5d0efd2
jordan <jordan.lesich@gmail.com>
2024-08-21 17:16:11 -0700

    setup script

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -4,9 +4,28 @@ pragma solidity ^0.8.13;
 import {Script, console} from "forge-std/Script.sol";
 
 contract CounterScript is Script {
-    function setUp() public {}
+    string _network;
+
+    uint256 constant FACILITATOR_HAT = 377439664716248287426848749960570468768546267599534423130416380641280;
+    address constant DAO = 0xb6FDF1C6a4D0EAd9aB894b5fAa48D82334C4e1e9;
 
     function run() public {
+        uint256 pk = vm.envUint("PRIVATE_KEY");
+        address deployer = vm.rememberKey(pk);
+        _setEnvString();
+
+        vm.startBroadcast(deployer);
+
         vm.broadcast();
+    }
+
+    function _setEnvString() internal {
+        uint256 key;
+
+        assembly {
+            key := chainid()
+        }
+
+        _network = vm.toString(key);
     }
 }
